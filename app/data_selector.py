5d2c21f0c1b3f158761294a7e6a9edb77669ada9
Vladyslav Pleshko <vldsv.pl1@gmail.com>
2026-05-11 17:33:23 +0300

    add active learning module: new step in deploy and selector.py

---
+++ app/data_selector.py
@@ -0,0 +1,34 @@
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+import os
+
+dynamodb = boto3.resource('dynamodb').Table("InferenceLogs")
+s3 = boto3.client('s3')
+
+def handler(event, context,
+            min_conf, max_conf):
+    # логіка вибору
+    response = dynamodb.query(
+        IndexName="ActiveLearningIndex",
+        KeyConditionExpression=Key('is_labeled').eq('False'),
+        FilterExpression=Attr('confidence').between('0.0', '0.6'),
+        Limit=20
+    )
+
+    items = response.get('Items', [])
+    for item in items:
+        # копіювання в S3
+        s3.copy_object(
+            CopySource={'Bucket':'aws-ml-logs', 'Key': item['s3_path']},
+            Bucket='aws-labeling-queue',
+            Key=f"pending/{item['prediction_id']}.jpg"
+        )
+
+        # оновлення статусу в DYNAMODB
+        dynamodb.update_item(
+            Key={'prediction_id': item['prediction_id'], 'timestamp': item['timestamp']},
+            UpdateExpression="SET is_labeled = :val",
+            ExpressionAttributeValues={':val': 'In_Review'}
+        )
+
+    return {'status': "success", "processed": len(items)}
