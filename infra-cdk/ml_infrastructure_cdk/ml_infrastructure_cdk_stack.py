059675d228c32aac31320dfee5623c96daebbbbe
Vladyslav Pleshko <vldsv.pl1@gmail.com>
2026-06-16 20:21:01 +0300

    build: created IaC

--- infra-cdk/ml_infrastructure_cdk/ml_infrastructure_cdk_stack.py
+++ infra-cdk/ml_infrastructure_cdk/ml_infrastructure_cdk_stack.py
@@ -1,106 +1,129 @@
 from aws_cdk import (
-    # Duration,
     Stack,
+    Duration,
+    RemovalPolicy,
     aws_ec2 as ec2,
     aws_rds as rds,
     aws_s3 as s3,
     aws_dynamodb as dynamodb,
     aws_lambda as _lambda,
-    RemovalPolicy
-    # aws_sqs as sqs,
+    aws_events as events,
+    aws_events_targets as targets,
 )
 from constructs import Construct
+
 
 class MlInfrastructureCdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Ізольована мережа VPC
+        # 1. МЕРЕЖЕВА ІНФРАСТРУКТУРА ТА БАЗА ДАНИХ (Нові ресурси)
+
+        # VPC для безпечного середовища MLflow та RDS
         self.vpc = ec2.Vpc(
             self, "MlOpsVpc",
             max_azs=2,
             subnet_configuration=[
                 ec2.SubnetConfiguration(
-                    name="Public",
-                    subnet_type=ec2.SubnetType.PUBLIC,
-                    cidr_mask=24
-                ),
+                    name="Public", subnet_type=ec2.SubnetType.PUBLIC, cidr_mask=24),
                 ec2.SubnetConfiguration(
-                    name="Private",
-                    subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS, # NAT Gateway
-                    cidr_mask=24
-                )
+                    name="Private", subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS, cidr_mask=24)
             ]
         )
-        self.mlflow_sg = ec2.SecurityGroup(
-            self, "MlflowSecurityGroup",
-            vpc=self.vpc,
-            description="Allow access to Mlflow server",
-            allow_all_outbound=True # Доступ до інтернету
-        )
-        self.mlflow_sg.add_ingress_rule(
-            peer=ec2.Peer.any_ipv4(),
-            connection=ec2.Port.tcp(5000),
-            description="Allow Web UI access"
-        )
 
-        # security for DB RDS
-        self.rds_sg = ec2.SecurityGroup(
-            self, "RdsSecurityGroup",
-            vpc=self.vpc,
-            description="Security group for RDS PostgreSQL",
-            allow_all_outbound=True
-        )
-        # дозвіл на вхідний трафік від Mlflow
-        self.rds_sg.add_ingress_rule(
-            peer=self.mlflow_sg, # бейдж mlflow
-            connection=ec2.Port.tcp(5432),
-            description="Allow PostgreSQL access from MLflow server"
-        )
-
-        # 2. Створення базу даних RDS PostgreSQL
-        self.postgres_db = rds.DatabaseInstance(
+        # База даних RDS PostgreSQL для метаданих MLflow
+        self.rds_db = rds.DatabaseInstance(
             self, "MlflowPostgresDB",
             engine=rds.DatabaseInstanceEngine.postgres(
-                version=rds.PostgresEngineVersion.VER_15
-            ),
+                version=rds.PostgresEngineVersion.VER_15),
             instance_type=ec2.InstanceType.of(
-                ec2.InstanceClass.BURSTABLE3,
-                ec2.InstanceSize.MICRO # t3.micro
-            ),
+                ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
             vpc=self.vpc,
-            security_groups=[self.rds_sg], # security group
             vpc_subnets=ec2.SubnetSelection(
-                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
-            ),
-            database_name="mlflow_db" # всередині системи
-        ),
-
-        # створення s3 bucket'y
-        self.model_bucket = s3.Bucket(
-            self, "MlflowModelBucket",
-            bucket_name="serverless-models-artifacts",
-            encryption=s3.BucketEncryption.S3_MANAGED, # шифрування даних
-            block_public_access=s3.BlockPublicAccess.BLOCK_ALL # повністю закритий доступ з інтернету
-        )
-
-        # -- МІГРАЦІЯ створених компонентів черех UI --
-        # імпорт існючого бакету за його назвою (той, що я створював в UI)
-        self.existing_photo_bucket = s3.Bucket.from_bucket_name(
-            self, "ExistingPhotoBucket",
-            bucket_name = "aws-ml-logs"
-        )
-        self.inference_logs_table = dynamodb.Table(
-            self, "InferenceLogsTable",
-            table_name="InferenceLogs", # навза у консолі
-            partition_key=dynamodb.Attribute(
-                name="prediction_id", # partititon key
-                type=dynamodb.AttributeType.STRING
-            ),
-            sort_key=dynamodb.Attribute(
-                name="timestamp", # sort key
-                type=dynamodb.AttributeType.STRING
-            ),
+                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
+            allocated_storage=20,
+            max_allocated_storage=100,
+            database_name="mlflow_db",
             removal_policy=RemovalPolicy.RETAIN
         )
+
+        # Новий бакет для артефактів моделей MLflow
+        self.model_bucket = s3.Bucket(
+            self, "MlflowArtifactsBucket",
+            bucket_name="serverless-models-artifacts",
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=RemovalPolicy.RETAIN,
+            auto_delete_objects=False
+        )
+
+        # 2. МІГРАЦІЯ ІСНУЮЧИХ РЕСУРСІВ
+
+        # Підключаємо існуючі S3 бакети
+        self.ml_logs_bucket = s3.Bucket.from_bucket_name(
+            self, "ExistingMlLogsBucket",
+            bucket_name="aws-ml-logs"
+        )
+
+        self.labeling_queue_bucket = s3.Bucket.from_bucket_name(
+            self, "ExistingLabelingQuequeBucket",
+            bucket_name="aws-labeling-queque"
+        )
+
+        # Описуємо ВЖЕ ІМПОРТОВАНУ таблицю DynamoDB
+        self.inference_logs_table = dynamodb.Table(
+            self, "InferenceLogsTable",
+            table_name="InferenceLogs",
+            partition_key=dynamodb.Attribute(
+                name="prediction_id", type=dynamodb.AttributeType.STRING),
+            sort_key=dynamodb.Attribute(
+                name="timestamp", type=dynamodb.AttributeType.STRING),
+            removal_policy=RemovalPolicy.RETAIN
+        )
+
+        # Lambda-функція інференсу (Docker-контейнер з кореня проєкту)
+        self.inference_lambda = _lambda.DockerImageFunction(
+            self, "MlEdgeInferenceLambda",
+            function_name="ml-serverless-inference",
+            code=_lambda.DockerImageCode.from_image_asset("."),
+            timeout=Duration.minutes(5),
+            memory_size=1024
+        )
+
+        # Lambda-функція Active Learning відбору (Docker-контейнер з кореня проєкту)
+        self.data_selector_lambda = _lambda.DockerImageFunction(
+            self, "MlDataSelectorLambda",
+            function_name="ml-serverless-data-selector",
+            code=_lambda.DockerImageCode.from_image_asset("."),
+            timeout=Duration.minutes(10),
+            memory_size=2048,
+            environment={
+                "LOGS_BUCKET": self.ml_logs_bucket.bucket_name,
+                "QUEUE_BUCKET": self.labeling_queue_bucket.bucket_name,
+                "DYNAMODB_TABLE": self.inference_logs_table.table_name
+            }
+        )
+
+        # 3. АВТОМАТИЗАЦІЯ ТА КЕРУВАННЯ ДОСТУПАМИ (IAM & Крон-тригери)
+
+        # правило EventBridge для запуску відбору щонеділі о 12:00 UTC
+        self.cron_rule = events.Rule(
+            self, "WeeklyDataSelectorTrigger",
+            rule_name="weekly-active-learning-selector",
+            schedule=events.Schedule.cron(
+                minute="0", hour="12", week_day="SUN"),
+            enabled=False
+        )
+
+        self.cron_rule.add_target(
+            targets.LambdaFunction(self.data_selector_lambda))
+
+        # права доступу
+        self.ml_logs_bucket.grant_read_write(self.inference_lambda)
+        self.inference_logs_table.grant_read_write_data(self.inference_lambda)
+
+        self.ml_logs_bucket.grant_read(self.data_selector_lambda)
+        self.inference_logs_table.grant_read_write_data(
+            self.data_selector_lambda)
+        self.labeling_queue_bucket.grant_read_write(self.data_selector_lambda)
